546ad270b35e04b9b4b70d63d8aa49de719c8acb
Iunir Iakupov <iunir.iakupov@gmail.com>
2021-10-30 08:57:56 +0300

    class 9

---
+++ 9/Laptop.py
@@ -0,0 +1,24 @@
+class Laptop:
+    def __init__(self, name: str, cpu_cores: int, core_power: float):
+        """
+        Constructor for Laptop class. Inits class properties
+
+        :param name: The Laptop brand name
+        :param cpu_cores: Number of CPU cores
+        :param core_power: Each CPU power in GHz
+        """
+        self.name = name
+        self.cpu_cores = cpu_cores
+        self.core_power = core_power
+
+    def total_cpu_power(self):
+        """
+        Calculates total CPU power by multiplication of core power and number of CPUs.
+        Lalala. Lalala. Lalala. Lalala. Lalala
+        :return: float: Total CPU power
+        """
+        self.__log("Total CPU power calculating")
+        return self.core_power * self.cpu_cores
+
+    def __log(self, value):
+        print(value)
